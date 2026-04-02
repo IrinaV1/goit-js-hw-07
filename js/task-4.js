18c7865d3d63394d7e9f4c4e3ffd43ba571634a4
Irina <vyustirina@gmail.com>
2026-04-02 21:37:08 +0300

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form")
+
+
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const forms = event.target.elements;
+  const email = forms.email.value.trim();
+  const password = forms.password.value.trim();
+  if (email === "" || password === "") {
+    return alert('All form fields must be filled in');
+  } 
+
+   console.log({
+    email: email,
+     password: password
+   });
+
+  event.target.reset();
+}
+form.addEventListener('submit', handleSubmit);
+
 
