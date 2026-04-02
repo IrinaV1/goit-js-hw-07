76ccf419d428fe06c194ffd74f9b4104d571f590
Irina <vyustirina@gmail.com>
2026-04-02 23:23:41 +0300

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,15 @@
+const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const spanColor = document.querySelector('.color');
+
+btn.addEventListener('click', handleClick);
+
+function handleClick() {
+  const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  spanColor.textContent = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
